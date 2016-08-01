42b022c1bce3eaa72762b36dd28358f149c961c8
Vinícius Soares <squimer@gmail.com>
2016-07-31 23:04:38 -0300

    Add tests that cover the basic flow of the GuideListPresenter.

--- app/src/test/java/br/com/mgalhardo/guidebook/ui/guidelist/GuideListPresenterTest.java
+++ app/src/test/java/br/com/mgalhardo/guidebook/ui/guidelist/GuideListPresenterTest.java
@@ -1,19 +1,33 @@
 package br.com.mgalhardo.guidebook.ui.guidelist;
 
+import com.google.gson.Gson;
+
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import br.com.mgalhardo.guidebook.domain.aggregation.GuideAggregation;
 import br.com.mgalhardo.guidebook.domain.repository.GuideRepository;
 import br.com.mgalhardo.guidebook.presentation.ui.guidelist.GuideListContract;
 import br.com.mgalhardo.guidebook.presentation.ui.guidelist.GuideListPresenter;
+import br.com.mgalhardo.guidebook.util.IOUtils;
 import br.com.mgalhardo.guidebook.util.RxSchedulersOverrideRule;
+import rx.Observable;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class GuideListPresenterTest {
 
     @Rule
     public final RxSchedulersOverrideRule rxJavaRule = new RxSchedulersOverrideRule();
+
+    Gson gson = new Gson();
 
     GuideRepository guideRepository;
     GuideListContract.View view;
@@ -29,4 +43,57 @@ public class GuideListPresenterTest {
         presenter.setView(view);
     }
 
+    @Test
+    public void loadGuidesFromRepositoryAndLoadIntoView() throws IOException {
+        String json = IOUtils.readStringFromResourcePath("upcoming-guides.json");
+        GuideAggregation guideAggregation = gson.fromJson(json, GuideAggregation.class);
+
+        when(guideRepository.getCompanies()).thenReturn(Observable.just(guideAggregation));
+
+        presenter.loadGuides();
+
+        verify(guideRepository).getCompanies();
+
+        verify(view).showLoadingLayout();
+        verify(view).showSuccessLayout();
+        verify(view).setupGuideList(guideAggregation.guides);
+
+        verifyNoMoreInteractions(guideRepository);
+        verifyNoMoreInteractions(view);
+    }
+
+    @Test
+    public void loadEmptyGuideListFromRepositoryAndPresentEmptyLayout() throws IOException {
+        GuideAggregation guideAggregation = new GuideAggregation();
+        guideAggregation.total = 0;
+        guideAggregation.guides = new ArrayList<>();
+
+        when(guideRepository.getCompanies()).thenReturn(Observable.just(guideAggregation));
+
+        presenter.loadGuides();
+
+        verify(guideRepository).getCompanies();
+
+        verify(view).showLoadingLayout();
+        verify(view).showEmptyLayout();
+
+        verifyNoMoreInteractions(guideRepository);
+        verifyNoMoreInteractions(view);
+    }
+
+    @Test
+    public void loadGuidesFromRepositoryFailsAndPresentErrorLayout() {
+        when(guideRepository.getCompanies())
+                .thenReturn(Observable.<GuideAggregation>error(new NullPointerException()));
+
+        presenter.loadGuides();
+
+        verify(guideRepository).getCompanies();
+
+        verify(view).showLoadingLayout();
+        verify(view).showErrorLayout();
+
+        verifyNoMoreInteractions(guideRepository);
+        verifyNoMoreInteractions(view);
+    }
 }
